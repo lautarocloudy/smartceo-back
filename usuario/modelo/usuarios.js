b956df73de1fc5bc9d770d2791dd8f5ce9898bf7
unknown <lautarorcloudy@gmail.com>
2024-05-17 17:39:25 -0300

    update

--- usuario/modelo/usuarios.js
+++ usuario/modelo/usuarios.js
@@ -5,6 +5,7 @@ const UserSchema = Schema({
     email: {type:String, require:true},
     password: {type:String, require:true},
     role:{type:String, require:true},
+    informe:{type:String},
 
 })
 
