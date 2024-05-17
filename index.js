11840c72233533eaaaaa0391f7913b430518df80
unknown <lautarorcloudy@gmail.com>
2024-05-17 17:22:28 -0300

    update

--- index.js
+++ index.js
@@ -3,33 +3,33 @@ const express = require("express");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
 
-// inicializar app
+// Inicializar app
 console.log("app de node arrancada");
 
-// conectar a la base de datos
+// Conectar a la base de datos
 conexion();
 
-// crear servidor node
+// Crear servidor node
 const app = express();
 const puerto = process.env.PORT || 3000;
 
 const corsOptions = {
   origin: "https://smart-ceo.netlify.app", // Reemplaza con tu dominio frontend
-  methods: "GET,POST,PUT,DELETE",
+  methods: "GET,POST,PUT,DELETE,OPTIONS",
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'X-Access-Token', 'Authorization'],
   credentials: true // Habilita las credenciales
 };
 
-// configurar cors
+// Configurar CORS
 app.use(cors(corsOptions));
 app.options("*", cors(corsOptions)); // Manejar solicitudes preflight globalmente
 
 app.use(fileUpload({
-    useTempFiles: true,
-    tempFileDir: './uploads'
+  useTempFiles: true,
+  tempFileDir: './uploads'
 }));
 
-// convertir body a objeto js
+// Convertir body a objeto JS
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -37,7 +37,7 @@ app.use(express.urlencoded({ extended: true }));
 const rutas_usuarios = require('./rutas/usuarios');
 app.use("/api/user", rutas_usuarios);
 
-// crear servidor y escuchar peticiones
+// Crear servidor y escuchar peticiones
 app.listen(puerto, () => {
-    console.log("servidor corriendo en el puerto " + puerto);
+  console.log("servidor corriendo en el puerto " + puerto);
 });
